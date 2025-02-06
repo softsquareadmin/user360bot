1903d784c6b09520bd95f42875de698bcf07d5ec
Manikandan P <manikandanp@softsquare.biz>
2025-02-06 19:57:31 +0530

    added new UI change

--- home.py
+++ home.py
@@ -19,7 +19,7 @@ from streamlit_cookies_controller import CookieController
 
 st.set_page_config(
     page_title="User360 - AI Chatbot",
-    page_icon="https://raw.githubusercontent.com/Maniyuvi/SoftsquareChatbot/main/SS512X512.png",
+    page_icon="https://raw.githubusercontent.com/softsquareadmin/chatbotgallery/main/User360.png",
     # initial_sidebar_state= 'expanded'
 )
 
@@ -361,7 +361,7 @@ with response_container:
     if st.session_state['responses']:
         for i in range(len(st.session_state['responses'])):
             response = f"<div style='font-size:0.875rem;line-height:1.75;white-space:normal;'>{st.session_state['responses'][i]}</div>"
-            message(response,allow_html=True,key=str(i),logo=('https://raw.githubusercontent.com/Maniyuvi/SoftsquareChatbot/main/SS512X512.png'))
+            message(response,allow_html=True,key=str(i),logo=('https://raw.githubusercontent.com/softsquareadmin/chatbotgallery/main/User360.png'))
             if i < len(st.session_state['requests']):
                 request = f"<meta name='viewport' content='width=device-width, initial-scale=1.0'><div style='font-size:.875rem'>{st.session_state['requests'][i]}</div>"
                 message(request, allow_html=True,is_user=True,key=str(i)+ '_user',logo='https://raw.githubusercontent.com/Maniyuvi/SoftsquareChatbot/main/generic-user-icon-13.jpg')
